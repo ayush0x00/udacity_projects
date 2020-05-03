d65328bea2dbc6105d383e7dc946292823e2fc46
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-05-03 14:04:33 +0530

    Completed Route planner project

--- Route_planning/src/main.cpp
+++ Route_planning/src/main.cpp
@@ -11,14 +11,14 @@
 using namespace std::experimental;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
+{
     std::ifstream is{path, std::ios::binary | std::ios::ate};
     if( !is )
         return std::nullopt;
-    
+
     auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
+    std::vector<std::byte> contents(size);
+
     is.seekg(0);
     is.read((char*)contents.data(), size);
 
@@ -28,7 +28,7 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 }
 
 int main(int argc, const char **argv)
-{    
+{
     std::string osm_data_file = "";
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
@@ -40,9 +40,9 @@ int main(int argc, const char **argv)
         std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
         osm_data_file = "../map.osm";
     }
-    
+
     std::vector<std::byte> osm_data;
- 
+
     if( osm_data.empty() && !osm_data_file.empty() ) {
         std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
@@ -51,16 +51,19 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x,start_y,end_x,end_y;
+    std::cout << "Enter the coordinates"<< '\n';
+    std::cin>>start_x>>start_y>>end_x>>end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x,start_y,end_x,end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
--- Route_planning/src/route_planner.cpp
+++ Route_planning/src/route_planner.cpp
@@ -10,6 +10,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    this->start_node=&model.FindClosestNode(start_x,start_y);
+    this->end_node=&model.FindClosestNode(end_x,end_y);
 
 }
 
@@ -19,20 +21,31 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
 
-float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 
+float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
+  node->distance(*end_node);
 }
 
 
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 // Tips:
 // - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
+// - For each node in current_node.neighbors, set the parent, the h_value, the g_value.
 // - Use CalculateHValue below to implement the h-Value calculation.
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+  current_node->FindNeighbors();
+  current_node->visited=true;
+  for (int i=0;i<current_node->neighbors.size();i++){
+    RouteModel:: Node* neighbor=current_node->neighbors[i];
+    neighbor->parent=current_node;
+    neighbor->g_value=current_node->g_value+neighbor->distance(*current_node);
+    neighbor->h_value=neighbor->distance(*end_node);
 
+    open_list.push_back(neighbor);
+    neighbor->visited=true;
+  }
 }
 
 
@@ -43,14 +56,23 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Remove that node from the open_list.
 // - Return the pointer.
 
+bool compare(RouteModel::Node*a, RouteModel::Node*b){
+  float f1=a->g_value + a->h_value;
+  float f2=b->g_value + b->h_value;
+  return f1>f2;
+}
+
 RouteModel::Node *RoutePlanner::NextNode() {
+  sort(this->open_list.begin(),open_list.end(),compare);
+  RouteModel:: Node*lowest_sum=open_list.back();
+  return lowest_sum;
 
 }
 
 
 // TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
 // Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
+// - This method should take the current (final) node as an argument and iteratively follow the
 //   chain of parents of nodes until the starting node is found.
 // - For each node in the chain, add the distance from the node to its parent to the distance variable.
 // - The returned vector should be in the correct order: the start node should be the first element
@@ -60,6 +82,13 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
+    while(current_node->x!=this->start_node->x && current_node->y!= this->start_node->y){
+      path_found.push_back(*current_node);
+      distance+=current_node->distance(*current_node->parent);
+      current_node=current_node->parent;
+    }
+    path_found.push_back(*current_node);
+    std::reverse(path_found.begin(),path_found.end());
 
     // TODO: Implement your solution here.
 
@@ -80,5 +109,15 @@ void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
     // TODO: Implement your solution here.
+    this->open_list.push_back(this->start_node);
+    while(this->open_list.size()>0){
+      current_node=this->NextNode();
+      this->open_list.pop_back();
+      if(current_node->x==this->end_node->x && current_node->y== this->end_node->y){
+        m_Model.path=this->ConstructFinalPath(current_node);
+        break;
+      }
+      AddNeighbors(current_node);
+    }
 
 }
